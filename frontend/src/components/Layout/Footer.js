9130a57d4ba2256300425d13831ed20a8a8728ae
abhaymishra <abhaymishra@nimapinfotech.com>
2021-11-19 14:12:59 +0530

    complete project dependency is changed from npm to yarn

---
+++ frontend/src/components/Layout/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import { Navbar, Nav, NavItem } from 'reactstrap';
+
+import SourceLink from 'components/SourceLink';
+
+const Footer = () => {
+  const yearNow = new Date().getFullYear();
+  return (
+    <div className="copyright text-center">&copy; Company - {yearNow}</div>
+  );
+};
+
+export default Footer;
---
+++ frontend/src/pages/ButtonGroupPage.js
@@ -0,0 +1,11 @@
+import Page from 'components/Page';
+import React from 'react';
+import { Button, ButtonGroup, ButtonToolbar, Card, CardBody, CardHeader, CardText, Col, DropdownItem, DropdownMenu, DropdownToggle, Row, UncontrolledButtonDropdown } from 'reactstrap';
+
+
+
+const ButtonGroupPage = () => {
+  return <h1>Partner</h1>;
+};
+
+export default ButtonGroupPage;
