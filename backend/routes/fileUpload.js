c488641ef6d0ca6f1cfb4e3c6a1ee33fc3d63d88
Bipin <bipin@nimapinfotech.com>
2021-11-19 14:21:19 +0530

    vendor update

--- backend/routes/fileUpload.js
+++ backend/routes/fileUpload.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router()
-const {ImageUpload} = require('../controllers/fileUploadController/fileUpload')
+const {ImageUpload} = require('../controllers/fileUpload/fileUpload')
 
 
 router.post("/",ImageUpload);
