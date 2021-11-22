4f3e85753c4a273934bae3347b83f0d4ac56030d
Bipin <bipin@nimapinfotech.com>
2021-11-22 18:02:10 +0530

    get and Update Donor by Id added.

--- backend/controllers/donors/donors.js
+++ backend/controllers/donors/donors.js
@@ -12,17 +12,53 @@ exports.getAllDonor = async (req, res) => {
     }
 }
 //Get api for getting parent details 
-exports.getAllParentDetails = async (req,res)=>{
+exports.getAllParentDetails = async (req, res) => {
     const data = await models.users.findAll({
-        attributes : ['name','id'] 
-        
+        attributes: ['name', 'id']
+
     })
-    if(!data){
-        return res.status(400).json({message : "Failed to get parent details"})
+    if (!data) {
+        return res.status(400).json({ message: "Failed to get parent details" })
     }
-    else{
+    else {
         return res.status(200).json({
-            data : data
+            data: data
         })
     }
+}
+
+exports.getDonorById = async (req, res) => {
+    let id = req.params.id
+    let data = await models.users.findByPk(id, { attributes: ['name', 'email', 'mobile', 'balance'] })
+    if (!data) {
+        return res.status(400).json({
+            message: "Failed to get Donor Details"
+        })
+    }
+    return res.status(200).json({
+        data: data
+    })
+}
+
+//Updating a Donor
+exports.updateDonor = async (req, res) => {
+    let id = req.params.id
+    let { name, number, email, plan, isPriyank, balanceNextRenewDate } = req.body;
+    let donorExists = await models.users.findOne({ where: { id: id } })
+    if (!donorExists) {
+        return res.status(400).json({
+            message: "Donor does not exists"
+        })
+    }
+
+    let donorUpdate = await models.users.update({ name, number, email, plan, isPriyank, balanceNextRenewDate }, { where: { id: id } })
+    if (!donorUpdate[0]) {
+        return res.status(401).json({
+            message: "Failed to update donor"
+        })
+    }
+    console.log(donorUpdate)
+    return res.status(200).json({
+        message: "Donor Updated Successfully",
+    })
 }
--- backend/routes/donor.js
+++ backend/routes/donor.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router()
 const {userRegister} = require('../controllers/auth/authController')
-const {getAllDonor,getAllParentDetails} =require('../controllers/donors/donors')
+const {getAllDonor,getAllParentDetails,getDonorById,updateDonor} =require('../controllers/donors/donors')
 const {wrapper} = require('../utils/errorWrap')
 const validationError =require('../middleware/validationError')
 const {donorValidation} =require('../validations/donors')
@@ -10,8 +10,10 @@ const {donorValidation} =require('../validations/donors')
 
 
 router.post('/',donorValidation,validationError,wrapper(userRegister))
-router.get("/",wrapper(getAllDonor))
+router.get('/',wrapper(getAllDonor))
 router.get('/parents',wrapper(getAllParentDetails))
+router.get('/:id',wrapper(getDonorById))
+router.put('/:id',wrapper(updateDonor))
 
 
 module.exports = router
