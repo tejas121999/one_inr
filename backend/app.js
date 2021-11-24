45aa3ca786138d24e268054610f64b15ee5656eb
Bipin <bipin@nimapinfotech.com>
2021-11-24 16:08:17 +0530

    cors added and implementing pagination.

--- backend/app.js
+++ backend/app.js
@@ -4,12 +4,13 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var dotenv = require('dotenv')
 dotenv.config({path : './.env'})
-
+const cors = require('cors')
 
 var app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
+app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
