d17c2aa78f73f7dd635f2a2fd1074e59e78a5cd1
tejasMetricoidtech <tejas@metricoidtech.com>
2021-11-25 14:41:24 +0530

    commit

--- frontend/src/Modals/Donor/AddFund.js
+++ frontend/src/Modals/Donor/AddFund.js
@@ -1,7 +1,32 @@
-import React from "react";
-import { Modal } from "react-bootstrap";
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import { Modal } from 'react-bootstrap';
+import { ADD_DONOR_FUND_URL, BASE_URL } from '../../API/APIEndpoints';
 
-const Addfund = (props) => {
+const Addfund = props => {
+  const [balance, setBalance] = useState(0);
+  const [userId, setId] = useState(0);
+
+  useEffect(() => {
+    setId(props.data);
+    console.log('useEffect', userId);
+  }, [props]);
+
+  const onSubmit = async () => {
+    const url = BASE_URL + `donor/balance/${userId}`;
+    console.log('ModalURL', url, balance);
+    const obj = {
+      balance: parseInt(balance),
+    };
+    await axios
+      .put(url, obj)
+      .then(res => {
+        console.log('fundAtt', res);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
   return (
     <React.Fragment>
       <Modal show={props.show} onHide={props.onHide}>
@@ -9,8 +34,17 @@ const Addfund = (props) => {
           <Modal.Title>Add Balance</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          <input type="number" className="form-control"></input>
-          <button style={{ margin: "20px" }} className="btn btn-primary">
+          <input
+            type="number"
+            className="form-control"
+            onChange={e => setBalance(e.target.value)}
+            value={balance}
+          ></input>
+          <button
+            style={{ margin: '20px' }}
+            className="btn btn-primary"
+            onClick={onSubmit}
+          >
             Add
           </button>
         </Modal.Body>
