2e1fa51c7f31cde223d2232809b9c35979d6b1c4
abhaymishra <abhaymishra@nimapinfotech.com>
2021-11-26 15:53:22 +0530

    ffff

--- frontend/src/pages/Doner/UpcomingDonerRenewal.js
+++ frontend/src/pages/Doner/UpcomingDonerRenewal.js
@@ -61,13 +61,13 @@ const UpcomingDonerRenewal = () => {
               <td>1</td>
               <td>
                 <Link to="#" type="" className="btn btn-link">
-                  <i className="fa fa-trash" aria-hidden="true"></i>
+                  <i class="fa fa-trash" aria-hidden="true"></i>
                 </Link>
-                <Link to="#" type="" className="btn btn-link">
-                  <i className="fa fa-eye" aria-hidden="true"></i>
+                <Link to="/view_doner" type="" className="btn btn-link">
+                  <i class="fa fa-eye" aria-hidden="true"></i>
                 </Link>
                 <Link to="/edit_doner" type="" className="btn btn-link">
-                  <i className="fa fa-pencil" aria-hidden="true"></i>
+                  <i class="fa fa-pencil" aria-hidden="true"></i>
                 </Link>
               </td>
             </tr>
@@ -79,14 +79,14 @@ const UpcomingDonerRenewal = () => {
               <td>1</td>
               <td>
                 <Link to="#" type="" className="btn btn-link">
-                  <i className="fa fa-trash" aria-hidden="true"></i>
+                  <i class="fa fa-trash" aria-hidden="true"></i>
                 </Link>
-                <Link to="#" type="" className="btn btn-link">
-                  <i className="fa fa-eye" aria-hidden="true"></i>
+                <Link to="/view_doner" type="" className="btn btn-link">
+                  <i class="fa fa-eye" aria-hidden="true"></i>
                 </Link>
 
-                <Link to="/editvendor" type="" className="btn btn-link">
-                  <i className="fa fa-pencil" aria-hidden="true"></i>
+                <Link to="/edit_doner" type="" className="btn btn-link">
+                  <i class="fa fa-pencil" aria-hidden="true"></i>
                 </Link>
               </td>
             </tr>
@@ -98,15 +98,14 @@ const UpcomingDonerRenewal = () => {
               <td>1</td>
               <td>
                 <Link to="#" type="" className="btn btn-link">
-                  <i className="fa fa-trash" aria-hidden="true"></i>
+                  <i class="fa fa-trash" aria-hidden="true"></i>
                 </Link>
-                <Link to="#" type="" className="btn btn-link">
-                  <i className="fa fa-eye" aria-hidden="true"></i>
+                <Link to="/view_doner" type="" className="btn btn-link">
+                  <i class="fa fa-eye" aria-hidden="true"></i>
                 </Link>
 
-<<<<<<< HEAD
-                <Link to="/editvendor" type="" className="btn btn-link">
-                  <i className="fa fa-pencil" aria-hidden="true"></i>
+                <Link to="/edit_doner" type="" className="btn btn-link">
+                  <i class="fa fa-pencil" aria-hidden="true"></i>
                 </Link>
               </td>
             </tr>
@@ -116,81 +115,5 @@ const UpcomingDonerRenewal = () => {
     </div>
   );
 };
-=======
-                <table className="table table-striped">
-                    <thead>
-                        <tr>
-                            <th>Renewal Date</th>
-                            <th>Name</th>
-                            <th>Donated</th>
-                            <th>Balance</th>
-                            <th>Project</th>
-                            <th>Action</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr >
-                            <th>17-aug-2020</th>
-                            <td>tejas talkar</td>
-                            <td>20</td>
-                            <td>30</td>
-                            <td>1</td>
-                            <td>
-                                <Link to='#' type="" className="btn btn-link">
-                                    <i class="fa fa-trash" aria-hidden="true"></i>
-                                </Link>
-                                <Link to='/view_doner' type="" className="btn btn-link">
-                                    <i class="fa fa-eye" aria-hidden="true"></i>
-                                </Link>
-                                <Link to='/edit_doner' type="" className="btn btn-link">
-                                    <i class="fa fa-pencil" aria-hidden="true"></i>
-                                </Link>
-                            </td>
-                        </tr>
-                        <tr>
-                            <th>17-aug-2020</th>
-                            <td>tejas talkar</td>
-                            <td>20</td>
-                            <td>30</td>
-                            <td>1</td>
-                            <td>
-                                <Link to='#' type="" className="btn btn-link">
-                                    <i class="fa fa-trash" aria-hidden="true"></i>
-                                </Link>
-                                <Link to='/view_doner' type="" className="btn btn-link">
-                                    <i class="fa fa-eye" aria-hidden="true"></i>
-                                </Link>
-                                
-                                <Link to='/edit_doner' type="" className="btn btn-link">
-                                    <i class="fa fa-pencil" aria-hidden="true"></i>
-                                </Link> 
-                            </td>
-                        </tr>
-                        <tr>
-                            <th>17-aug-2020</th>
-                            <td>tejas talkar</td>
-                            <td>20</td>
-                            <td>30</td>
-                            <td>1</td>
-                            <td>
-                                <Link to='#' type="" className="btn btn-link">
-                                    <i class="fa fa-trash" aria-hidden="true"></i>
-                                </Link>
-                                <Link to='/view_doner' type="" className="btn btn-link">
-                                    <i class="fa fa-eye" aria-hidden="true"></i>
-                                </Link>
-                                
-                                <Link to='/edit_doner' type="" className="btn btn-link">
-                                    <i class="fa fa-pencil" aria-hidden="true"></i>
-                                </Link>
-                            </td>
-                        </tr>
-                    </tbody>
-                </table>
-            </div>
-        </div>
-    )
-}
->>>>>>> f034b8621929b76c9939beb94922835e5166429b
 
 export default UpcomingDonerRenewal;
