be78d233b8554e21173289c0ac91d1fa4159c4aa
abhaymishra <abhaymishra@nimapinfotech.com>
2021-11-26 10:12:12 +0530

    ngrok link add

--- frontend/src/API/APIEndpoints.js
+++ frontend/src/API/APIEndpoints.js
@@ -1,4 +1,4 @@
-export const BASE_URL = 'http://5ba8-110-227-211-43.ngrok.io/api/';
+export const BASE_URL = ' http://ab1e-110-227-211-43.ngrok.io/api/';
 export const ADD_DONOR_GET_PARENTS_URL = 'donor/parents/';
 export const ADD_DONOR_URL = 'donor';
 export const ADD_DONOR_FUND_URL = 'donor/balance';
