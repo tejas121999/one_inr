0c149a1ad00fb143412e7983ed5ea2259ab04f31
Akshay Eklare <akshakumareklare@gmail.com>
2021-12-04 14:41:50 +0530

    ngo and ngo bank details

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -7,6 +7,7 @@ const auth = require('./auth') //Importing Authorization Module
 const donor = require('./donor') //Importing Donor Module 
 const userReceipts = require('./usersReceipts')//importing User Reciepts
 const ngo = require('./ngo')//importing ngo module
+const ngoBankDetails = require('./ngoBankDetails')
 
 
 router.use('/vendor',vendor)
@@ -15,5 +16,6 @@ router.use('/auth',auth)
 router.use('/donor',donor)
 router.use('/userReceipts',userReceipts)
 router.use('/ngo',ngo)
+router.use('/ngoBankDetails',ngoBankDetails)
 
 module.exports = router;
--- backend/routes/ngo.js
+++ backend/routes/ngo.js
@@ -5,17 +5,19 @@ var router = express.Router();
 
 const { wrapper } = require('../utils/errorWrap');
 
-const ngoController = require('../Controllers/ngo');//importing ngo controller
-
 const validationError = require('../middleware/validationError'); //importing validatation error from middleware
 
 const { ngoValidation } = require('../validations/ngo');//importing ngo validatation
 
 const { addNgo, updateNgo, getAllNgo, deleteNgo } = require('../Controllers/ngo');//importing ngo controller operation from controller
 
+const { addNgoBankDetails } = require('../Controllers/ngoBankDetails')//Importing Ngo Bank Details from Controllers
+
 //routing modules
 
 router.post('/create-ngo', wrapper(addNgo))//creating ngo 
+
+router.post('/create-bank-details', addNgoBankDetails)//creating ngo bank details
 
 router.get('/read-ngo', wrapper(getAllNgo))//listing ngo
 
