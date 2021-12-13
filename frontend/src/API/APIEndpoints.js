65019ab395ad724366105f48da18e740622b11ed
chinmayanandNimap <chinmayanand@nimapinfotech@gmail.com>
2021-12-13 10:51:31 +0530

    After some changes

--- frontend/src/API/APIEndpoints.js
+++ frontend/src/API/APIEndpoints.js
@@ -1,4 +1,4 @@
-export const Local = 'http://3fc5-122-179-162-41.ngrok.io';
+export const Local = 'http://newoneinr.nimapinfotech.com/';
 export const BASE_URL = `${Local}/api/`;
 export const BASE_URL_RECEIPT =
   'http://newoneinr.nimapinfotech.com/api/userReceipts';
