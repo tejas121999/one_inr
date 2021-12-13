20259ed6358b9154d1134e59610e5cfb4aef9c65
Bipin <bipin@nimapinfotech.com>
2021-12-13 16:34:08 +0530

    checkAuth reintegrated, get all parent updated. Forgot password integrated with token.

--- backend/utils/tokens.js
+++ backend/utils/tokens.js
@@ -1,18 +1,28 @@
 const jwt = require('jsonwebtoken');
 
-
-
-
-
 //Creating JSON WEB TOKEN where it will take a user Id.
-exports.generateJwtToken =(id) => {
-    const token = jwt.sign({id},process.env.TOKEN_SECRET_KEY,{expiresIn: process.env.TOKEN_EXPIRES_TIME})
+exports.generateJwtToken = (id) => {
+    const token = jwt.sign({ id }, process.env.TOKEN_SECRET_KEY, { expiresIn: process.env.TOKEN_EXPIRES_TIME })
+    return token;
+}
+exports.generateJwtTokenForOTP = (email, otp) => {
+    const token = jwt.sign({ email, otp }, process.env.TOKEN_SECRET_KEY, { expiresIn: process.env.OTP_TOKEN_EXPRES_TIME })
     return token;
 }
 
-//Verifying JSON WEB TOKEN
-exports.verifyJwtToken = (token)=>{
-    const decodedData = jwt.verify(token,process.env.TOKEN_SECRET_KEY)
-    
 
+//Verifying JSON WEB TOKEN
+exports.verifyJwtToken = (token) => {
+    const decodedData = jwt.verify(token, process.env.TOKEN_SECRET_KEY)
+    return decodedData;
+
+}
+//Verifying OTP in JSON WEB TOKEN
+exports.verifyOtpToken = async (token) => {
+    try {
+        const decodedData = jwt.verify(token, process.env.TOKEN_SECRET_KEY)
+        return decodedData;
+    } catch (err) {
+        return { message: 'OTP Expired' };
+    }
 }
