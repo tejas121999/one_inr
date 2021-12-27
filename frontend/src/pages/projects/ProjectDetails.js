567603a3f4b15340665c6717b514183fb9d70c6d
tejasMetricoidtech <tejas@metricoidtech.com>
2021-12-27 11:32:10 +0530

    commit

--- frontend/src/pages/projects/ProjectDetails.js
+++ frontend/src/pages/projects/ProjectDetails.js
@@ -1,6 +1,14 @@
 import React from 'react'
+import { Carousel, Nav, Tab, Tabs } from 'react-bootstrap'
+import { useHistory } from 'react-router-dom';
+
 
 const ProjectDetails = () => {
+    const history = useHistory();
+    const [key, setKey] = React.useState('details');
+    const [projectkey, setProjectKey] = React.useState('project_Details');
+
+
     return (
         <div>
             <br />
@@ -31,9 +39,51 @@ const ProjectDetails = () => {
                 <div className='white-box'>
                     <div className='row'>
                         <div className='col-mb-12'>
-                            <ul className='nav nav-pills m-b-30'>
-                            
-                            </ul>
+                            <Tabs
+                                id="controlled-tab-example"
+                                activeKey={key}
+                                onSelect={(k) => setKey(k)}
+                                className="mb-3"
+                            >
+                                <Tab eventKey="details" title="Details" >
+                                    <div className='row'>
+                                        <div className='col-4'>
+                                            <Carousel nextLabel={null} prevLabel={null}>
+
+                                            </Carousel>
+                                        </div>
+
+                                        <div className='col-8'>
+                                            <br />
+                                            <br />
+                                            <Tabs
+                                                id="controlled-tab-example"
+                                                activeKey={projectkey}
+                                                onSelect={(k) => setProjectKey(k)}
+                                                className="mb-3"
+                                            >
+                                                <Tab eventKey="project_Details" title="Project Details" >
+                                                    <p className='m-t-30'>1INR has collaborated with Hunger Project by Lions Club of Millennials and
+                                                        Lions Club of Churchgate Mumbai supported by Roti Bank.</p>
+                                                    <b>
+                                                        About Project
+                                                        <br />
+                                                    </b>
+                                                    <p className='p1'>
+                                                        <span style={{}}></span>
+                                                    </p>
+                                                </Tab>
+                                            </Tabs>
+                                        </div>
+                                    </div>
+                                </Tab>
+                                <Tab eventKey="contributors" title="Contributors" >
+                                    <br />
+                                    <div>
+                                        <h1 align={'center'}> No Contribution Yet </h1>
+                                    </div>
+                                </Tab>
+                            </Tabs>
                         </div>
                     </div>
                 </div>
