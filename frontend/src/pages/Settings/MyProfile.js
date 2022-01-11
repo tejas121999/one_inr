95ee2d92a00c54f2ef3f77ee3d9840f85fc21aec
tejasMetricoidtech <tejas@metricoidtech.com>
2022-01-11 13:28:14 +0530

    commit

--- frontend/src/pages/Settings/MyProfile.js
+++ frontend/src/pages/Settings/MyProfile.js
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { getProfileAction } from '../../Redux/Actions/SettingAction'
+import { Local } from '../../API/APIEndpoints';
+
 
 
 const MyProfile = () => {
