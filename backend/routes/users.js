87bcf9962ba04d6cd3fc29048ce7a521121f6af7
Bipin <bipin@nimapinfotech.com>
2022-01-12 12:04:31 +0530

    roles get api created.

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -3,11 +3,11 @@ var router = express.Router();
 const checkAuth = require('../middleware/checkAuth')
 const {myProfile,updateProfile,updateProfilePassword,createUser} = require('../controllers/myProfile')
 const { updateProfileValidation,updateProfilePasswordValidation} = require('../validations/user')
+const {wrapper} = require('../utils/errorWrap');
 
-
-router.get('/my-profile',checkAuth,myProfile)
-router.put('/update',checkAuth,updateProfileValidation,updateProfile)
-router.put('/update/password',checkAuth,updateProfilePasswordValidation,updateProfilePassword)
-router.post('/',checkAuth,createUser)
+router.get('/my-profile',checkAuth,wrapper(myProfile))
+router.put('/update',checkAuth,updateProfileValidation,wrapper(updateProfile))
+router.put('/update/password',checkAuth,updateProfilePasswordValidation,wrapper(updateProfilePassword))
+router.post('/',checkAuth,wrapper(createUser))
 
 module.exports = router;
